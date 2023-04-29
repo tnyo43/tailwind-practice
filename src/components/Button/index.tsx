4c69394c86089e0c6da7251be2f2a02e91cc75f0
Tomoya Kashifuku <tnyo43@gmail.com>
2023-04-29 16:06:45 +0900

    setup colors

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,10 +6,10 @@ type Props = React.ComponentPropsWithoutRef<"button"> & {
 
 export const Button: React.FC<Props> = ({ theme = "default", ...props }) => (
   <button
-    className={`h-7 rounded-lg  px-4
-    ${theme === "default" ? "bg-gray-light text-gray-dark" : undefined}
-    ${theme === "accent" ? "bg-blue text-white" : undefined}
-    ${theme === "alert" ? "bg-white text-orange" : undefined}`}
+    className={`h-7 rounded-lg border px-4
+    ${theme === "default" ? "bg-mono-background text-mono" : undefined}
+    ${theme === "accent" ? "bg-accent-background text-accent" : undefined}
+    ${theme === "alert" ? "bg-alert-background text-alert" : undefined}`}
     {...props}
   />
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,55 @@
+const colors = {
+  red: {
+    10: "#ffebeb",
+    20: "#ffd6d6",
+    30: "#ffc2c2",
+    40: "#ffadad",
+    50: "#f99",
+    60: "#ff8585",
+    70: "#ff7070",
+    80: "#ff5c5c",
+    90: "#ff3939",
+    100: "#f33",
+  },
+  green: {
+    10: "#ebffeb",
+    20: "#d6ffd6",
+    30: "#c2ffc2",
+    40: "#adffad",
+    50: "#9f9",
+    60: "#85ff85",
+    70: "#70ff70",
+    80: "#5cff5c",
+    90: "#39ff39",
+    100: "#3f3",
+  },
+  blue: {
+    10: "#ebebff",
+    20: "#d6d6ff",
+    30: "#c2c2ff",
+    40: "#adadff",
+    50: "#99f",
+    60: "#8585ff",
+    70: "#7070ff",
+    80: "#5c5cff",
+    90: "#3939ff",
+    100: "#33f",
+  },
+  gray: {
+    10: "#ebebeb",
+    20: "#d6d6d6",
+    30: "#c2c2c2",
+    40: "#adadad",
+    50: "#999",
+    60: "#858585",
+    70: "#707070",
+    80: "#5c5c5c",
+    90: "#393939",
+  },
+  white: "#fff",
+  black: "#333",
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,16 +59,19 @@ module.exports = {
   ],
   theme: {
     colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-      white: "#fff",
+      ...colors,
+      alert: {
+        DEFAULT: colors.red[100],
+        background: colors.red[10],
+      },
+      accent: {
+        DEFAULT: colors.blue[100],
+        background: colors.blue[10],
+      },
+      mono: {
+        DEFAULT: colors.black,
+        background: colors.gray[10],
+      },
     },
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
